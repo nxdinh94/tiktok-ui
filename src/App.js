d5a5c97031bd14f029080f7d5232bba1dbd76469
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-08-28 09:51:21 +0700

    Configure routerr and build Layout for project tiktok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './Routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
-  return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
-  );
+    console.log(publicRoutes);
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) Layout = Fragment;
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page /> {/* will be children of <Layout> */}
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,10 @@
+
+function Upload() {
+    return (
+            <div className="content">
+                <h2>Upload page</h2>
+            </div>
+    );
+}
+
+export default Upload;
